e86bd169153fea56e5477704169c1378dc488015
webeweb <support@webeweb.fr>
2021-07-16 15:05:12 +0200

    Code checkstyle

--- tests/Serializer/JsonSerializerHelperTest.php
+++ tests/Serializer/JsonSerializerHelperTest.php
@@ -5,7 +5,7 @@
  *
  * (c) 2021 WEBEWEB
  *
- * For the full copyright and license information"; const LANG_ = "please view the LICENSE
+ * For the full copyright and license information please view the LICENSE
  * file that was distributed with this source code.
  */
 
